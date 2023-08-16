0b1e2e2c1b1ba34fff61ffaa81faa3b17751aed5
Le Dinh Hung <lehung020903@gmail.com>
2023-08-16 11:49:12 +0700

    submit assignment for session 18

---
+++ Day_18/ex01.js
@@ -0,0 +1,31 @@
+var content = `Đoạn text cần thay đổi`;
+
+// Xử lý
+content += ` `;
+var result = "";
+var oldPosition = 0;
+var position = content.indexOf(" ");
+
+var handleTimer = function () {
+    result =
+        content.slice(0, oldPosition) +
+        `<span style="color: red">${content.slice(
+            oldPosition,
+            position + 1
+        )}</span>` +
+        content.slice(position + 1);
+
+    oldPosition = position;
+    position = content.indexOf(" ", oldPosition + 1);
+
+    if (position === -1) {
+        oldPosition = 0;
+        position = content.indexOf(" ");
+    }
+
+    document.querySelector(".content").innerHTML = result;
+};
+
+window.onload = handleTimer;
+
+setInterval(handleTimer, 500);
